24ad546e9588f4802c9960c3452e9c47a26b20e1
Julredo <43213385+Julredo@users.noreply.github.com>
2018-09-28 00:41:26 -0300

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preguntas frecuentes</title>
+      <link rel="stylesheet" href="css/faqCSS.css">
+  </head>
+  <body>
+    <h3 class="titulo" >Preguntas Frecuentes</h3>
+    <div class="accordion vertical">
+        <ul>
+            <li>
+                <input type="checkbox" id="checkbox-1" name="checkbox-accordion" />
+                <label for="checkbox-1">¿Pregunta&nbsp;1?</label>
+                <div class="content">
+                    <h3>Respuesta simple</h3>
+                    <p>Detalles</p>
+                </div>
+            </li>
+            <li>
+                <input type="checkbox" id="checkbox-2" name="checkbox-accordion" />
+                <label for="checkbox-2">¿Pregunta&nbsp;2?</label>
+                <div class="content">
+                    <h3>Respuesta simple</h3>
+                    <p>Detalles</p>
+                </div>
+            </li>
+            <li>
+                <input type="checkbox" id="checkbox-3" name="checkbox-accordion" />
+                <label for="checkbox-3">¿Pregunta&nbsp;3?</label>
+                <div class="content">
+                    <h3>Respuesta simple</h3>
+                    <p>Detalles</p>
+                </div>
+            </li>
+            <li>
+                <input type="checkbox" id="checkbox-4" name="checkbox-accordion" />
+                <label for="checkbox-4">¿Pregunta&nbsp;4?</label>
+                <div class="content">
+                    <h3>Respuesta simple</h3>
+                    <p>Detalles.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+  </body>
+</html>
