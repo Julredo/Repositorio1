b5db521361438270bd2852f785d293737f8df787
Julredo <43213385+Julredo@users.noreply.github.com>
2018-10-03 00:43:45 -0300

    Footer

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>footer</title>
+    <link rel="stylesheet" href="css/footerCSS.css">
+  </head>
+  <body>
+
+<!-- texto de prueba -->
+
+<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+
+<!-- fin texto prueba -->
+
+
+<div class="footer">
+  <footer>
+    <div>
+  <p>Nuestras redes</p>
+</div>
+<div class="social">
+
+  <!-- Logo. Deberia linkear a home -->
+  <a href="https://www.google.com/" class="logo"   ><img src="images/dado.png" alt="" height="32" ></a>
+  <!-- fin link -->
+  <a href="https://www.facebook.com/" class="icono"> <img src="images/facebookNegro.png" alt="" height="32" >   </a>
+  <a href="https://twitter.com" class="icono"><img src="images/twitterNegro.png" alt="" height="32" ></a>
+  <a href="https://www.instagram.com" class="icono"><img src="images/instagramNegro.png" alt="" height="32" ></a>
+</div>
+  </footer>
+</div>
+  </body>
+</html>
